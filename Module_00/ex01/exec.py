485f12f582806320bfe69b3b630f1b76e0dd02c2
calle <calle@student.42.fr>
2022-05-19 16:27:10 +0200

    updating ex01 to PEP8 norm

--- Module_00/ex01/exec.py
+++ Module_00/ex01/exec.py
@@ -1,6 +1,7 @@
 import sys
-#Make a program that takes a string as argument, reverses it, swaps its letters case and print the result.
-res =''
+# Make a program that takes a string as argument, reverses it,
+# swaps its letters case and print the result.
+res = ''
 if len(sys.argv) == 1:
     print('usage: python exec.py arg1 [arg2 ... argn]')
     exit(1)
@@ -8,7 +9,7 @@ if len(sys.argv) == 1:
 nbrOfArguments = len(sys.argv)
 for i in range(1, nbrOfArguments):
     res += sys.argv[i]
-    if nbrOfArguments != 2 and i < nbrOfArguments -1:
+    if nbrOfArguments != 2 and i < nbrOfArguments - 1:
         res += " "
 res = res.swapcase()
 print(res[::-1])
