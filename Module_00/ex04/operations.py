600135c350c3cae89e04d384889aec88e3f84abc
cnstll <calle@student.42.fr>
2022-05-20 16:15:45 +0200

    Done and dusted

--- Module_00/ex04/operations.py
+++ Module_00/ex04/operations.py
@@ -3,7 +3,7 @@ import sys
 
 def check_arg(arg):
     if len(arg) > 3:
-        print('InputError: too many arguments')
+        print('InputError: too many arguments.')
         exit(1)
     elif len(arg) < 2:
         print('USAGE: : python operations.py <number1> <number2>')
@@ -11,14 +11,21 @@ def check_arg(arg):
 
 
 def do_operations(lhs, rhs):
-    print("Sum:        %d" % lhs + rhs)
-    print("Difference: %d" % lhs - rhs)
-    print("Product:    %d" % lhs * rhs)
-    print("Quotient:   %d" % lhs / rhs)
-    print("Remainder:  %d" % lhs % rhs)
-    
+    try:
+        print("Sum:        %d" % (lhs + rhs))
+        print("Difference: %d" % (lhs - rhs))
+        print("Product:    %d" % (lhs * rhs))
+        print("Quotient:   %f" % (lhs / rhs))
+        print("Remainder:  %d" % (lhs % rhs))
+    except ZeroDivisionError:
+        print("Quotient:   %s" % ("ERROR (div by zero)"))
+        print("Remainder:  %s" % ("ERROR (modulo by zero)"))
+
+
 check_arg(sys.argv)
-print(type(sys.argv[1]))
-lhs = int(sys.argv[1])
-rhs = int(sys.argv[2])
-# do_operations(lhs,rhs)
+try:
+    lhs = int(sys.argv[1])
+    rhs = int(sys.argv[2])
+    do_operations(lhs, rhs)
+except ValueError:
+    print("InputError: only numbers.")
